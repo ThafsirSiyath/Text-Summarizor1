e90decf0d848cb9c9f8517ecfd222cd8f3eb2453
Siyat007 <thafsirsiyath22@gmail.com>
2025-09-17 11:27:41 +0530

    chages made

--- Template.py
+++ Template.py
@@ -7,6 +7,7 @@ logging .basicConfig(level=logging.INFO,format="[%(asctime)s]: %(message)s:")
 project_name="TextSummarizer"
 
 List_of_files=[
+    ".github/workflows/.gitkeep",
      f"src/{project_name}/__init__.py",
      f"src/{project_name}/components/__init__.py",
      f"src/{project_name}/utills/__init__.py",
